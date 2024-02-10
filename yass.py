fa481d88c2214b4676e8926814bf1a2545b953c5
Ivan Danchuk <ivan.s.danchuk@gmail.com>
2024-02-10 12:37:10 +0300

    refactor(core): the behavior of the Yass class has been changed
    The main class now has the functionality of
    a scraper factory and independently processes
    the trigger signal cycle.

--- yass.py
+++ yass.py
@@ -1,75 +1,63 @@
-import importlib
-import os.path
-import pkgutil
-from asyncio import create_task, gather, run
+from asyncio import FIRST_COMPLETED, Task, create_task, run, wait
 from collections import defaultdict
+from dataclasses import dataclass, field
 from itertools import chain
-from typing import ClassVar
+from typing import ClassVar, Literal
 
-import dotted_dict
-from attrs import define, field
-
-import base
+from scrapers import ApiScraper
+from triggers import conditions_instances
 
 
-@define(slots=False)
+@dataclass
 class Yass:
     attr_mapping: ClassVar[dict] = field(default=defaultdict(list))
-    context_registry: dict = field(factory=dict)
-    is_load_modules: bool = field(default=False)
+    lookup_interval: int = field(default=600)
 
-    def __attrs_post_init__(self):
-        # TODO: нужно оттестировать хорошенько данную часть, чтобы она было более универсальна
-        if not self.is_load_modules:
-            modules_path = os.path.curdir
-            for pkg in pkgutil.iter_modules([modules_path]):
-                importlib.import_module(pkg.name) if not pkg.name.startswith(
-                    "test_"
-                ) else None
-            self.is_load_modules = True
-
-    def create_context(self):
-        ctx = base.Context()
-        self.context_registry[id(ctx)] = ctx
-        return base.EnvBuilder(ctx)
-
-    async def run_coros(self):
-        containers = chain(
-            *[getattr(self, attr) for attr in self.attr_mapping.keys()]
-        )
-        tasks = [create_task(coro.pending()) for coro in containers]
-        await gather(*tasks)
-
-    def run(self):
-        self.__prepare_app()
-        run(self.run_coros())
-
-    def __prepare_app(self):
-        self.__build_attr_mapping()
-        if not self.attr_mapping.keys():
-            raise AttributeError(
-                "Не установлено ни одного аттрибута. Возможно, не иницилизирован ни один подкласс YassActor"
+    def make_crawl(self, url: str, *, crawl_type: Literal["api", "raw"]):
+        if crawl_type == "api":
+            return ApiScraper(url)
+        if crawl_type == "raw":
+            msg = (
+                "Скрапер для ресурсов без API в настоящее время не реализован"
             )
-        for attr, collection in self.attr_mapping.items():
-            setattr(self, attr, collection)
+            raise NotImplementedError(msg) from None
 
-    def __build_attr_mapping(self):
-        if len(self.context_registry.values()) < 1:
-            raise RuntimeError(
-                "Не создано ни одного юнита работы, создайте хотя бы один контекст работы"
-            )
-        contexts = list(self.context_registry.values())
-        instances_lst = [
-            c
-            for c in chain(*[list(v.values()) for v in contexts])
-            if isinstance(c, base.YassActor)
-        ]
-        d = defaultdict(list)
-        mapping = {
-            d[f"{bcls.__class__.__name__.lower()}s"].append(bcls)
-            for bcls in instances_lst
+    async def _run_coros(self) -> None:
+        # Мы запускаем все триггеры на ожидание в конкрутентом исполнении и рекурсивно их перезапускаем
+        awaiting_tasks = {
+            create_task(cond.pending()) for cond in conditions_instances.keys()
         }
-        self.attr_mapping = d
+        while awaiting_tasks:
+            done, pending = await wait(
+                awaiting_tasks,
+                timeout=self.lookup_interval,
+                return_when=FIRST_COMPLETED,
+            )
+            print(f"Done is {done}, pending is {pending}")
+            awaiting_tasks: set[Task] = pending
+            for task in done:
+                if task.exception() is None:
+                    key = task.result()
+                    if key in conditions_instances.keys():
+                        callback: Task = create_task(
+                            conditions_instances[key]()
+                        )
+                        print(f"Triggered callback is {callback}")
+                        awaiting_tasks.add(callback)
+                        print(f"Awaiting tasks is {awaiting_tasks}")
+                else:
+                    # TODO: здесь будут обрабатываться ошибки, которые можно исправить
+                    # и перезагрузить исполнение
+                    print(
+                        f"Condition {task.get_coro()} finished execution with an error {task.exception()}"
+                    )
+                    task.cancel()
+                    await task
+        print("Start pending another conditions cycle")
+        return await self._run_coros()
+
+    def run(self) -> None:
+        run(self._run_coros())
 
 
 if __name__ == "__main__":
