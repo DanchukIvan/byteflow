99bacaf63a41f799e704e44c34462d553f040018
Ivan Danchuk <ivan.s.danchuk@gmail.com>
2024-02-10 13:08:59 +0300

    refactor(scrapers): the functionality of all classes of the Scraper type has been changed.
    New scraper functionality:
    - designing a proxy URL
    - automatic creation of an instance of a
    scraper-compatible resource class
    - processing requests to the resource has been
    put into a separate method
    - scrapers now intercept the EndOfResource
    exception

--- scrapers.py
+++ scrapers.py
@@ -1,95 +1,139 @@
-from abc import ABC, abstractmethod
-from contextlib import suppress
-from datetime import datetime
-from urllib.parse import urlparse
+from abc import abstractmethod
+from asyncio import gather, sleep
+from collections.abc import AsyncGenerator, Awaitable, Callable, Coroutine
+from contextlib import asynccontextmanager
+from dataclasses import KW_ONLY, InitVar, dataclass, field
+from itertools import filterfalse
+from random import choice
+from typing import Any, Self, TypeVar
 
-from attrs import define, field, validators
-from httpx import AsyncClient
+import aiohttp
+from aiohttp import ClientResponse, ClientSession
+from aioitertools.more_itertools import chunked
+from yarl import URL
 
-import base
+from base import YassCore
 from constants import NOTHING
-from helpers import mark_as_trigger
-from repo import LocalRepo, NetworkRepo, Repo, SqlRepo
-from repo_utils_io import ReadHandler
-from resources import Resource
+from exceptions import EndOfResource
+from repo import RepoManager
+from resources import ApiResource, Resource
+from triggers import as_trigger
 
-scrapers_registry = {}
+# TODO: перенести в специальные переменные
 
-from typing import ClassVar
+S = TypeVar("S", bound="Scraper")
+
+proxy_list: list = []
 
 
-@define(slots=False)
-class Scraper(ABC, base.YassActor):
-    storage: Repo = field(default=NOTHING)
-    resource: Resource = field(default=NOTHING)
-    extra_headers: dict = field(default={})
-    meta: str = field(default="scrapers")
-    content_handler: ClassVar["ReadHandler"] = ReadHandler()
+# TODO: метод для скрапера "сырых" ресурсов
+async def process_requests(self: Self, urls: list[str]) -> list[bytes]:
+    if not proxy_list:
+        proxy_list = [None] * len(urls)
+    targets = zip(urls, proxy_list)
+    session: type[ClientSession]
+    async with self.client_factory(
+        timeout=self.req_timeout, headers=self.extra_headers
+    ) as session:
+        tasks: list[Coroutine[Awaitable[Any], None, ClientResponse]] = [
+            session.get(url, proxy=proxy) for url, proxy in targets
+        ]  # type: ignore
+        responses: list[ClientResponse] = await gather(*tasks)
+    error_resp: ClientResponse
+    for error_resp in filterfalse(
+        lambda x: x.status in list(range(200, 299, 1)), responses
+    ):
+        msg: str = (await error_resp.content.read()).decode()
+        # TODO: нужно подумать как мы будем кэтчить ошибки - если режим нестрогий, то можно только логировать ошибки подробно и усе
+        raise RuntimeError(error_resp.url, error_resp.headers, msg)
+    contents: list[bytes] = await gather(
+        *[resp.content.read() for resp in responses]
+    )
+    return contents
 
-    # def __attrs_pre_init__(self, **kwargs):
-    #     super().__init__(kwargs)
+
+@dataclass
+class Scraper(YassCore):
+    url: InitVar[str]
+    resource: Resource = field(init=False)
+    _: KW_ONLY
+    extra_headers: dict = field(default_factory=dict)
+    storage: RepoManager = field(default_factory=RepoManager)
+    req_timeout: int = field(default=10)
+    req_delay: float = field(default=0.5)
 
     @abstractmethod
-    def scrape(self):
+    async def scrape(self):
         """Основной метод скрапинга, частный метод реализации которого зависит от типа паука"""
 
-    @abstractmethod
-    def pending(self):
-        ...
+    def build_proxyurl(
+        *,
+        address: str = "",
+        port: int | None = None,
+        username: str = "",
+        password: str = "",
+        display_url: bool = False,
+    ) -> str:
+        url_string: URL = (
+            URL(address)
+            .with_port(port)
+            .with_password(password)
+            .with_user(username)
+        )
+        proxy_list.append(url_string.human_repr())
+        if display_url:
+            print(f"Prepared proxy url {url_string.human_repr()}")
+        return url_string.human_repr()
 
-    def make_path(self, storage):
-        if (strg_cls := storage.__class__) in (NetworkRepo, LocalRepo):
-            domain = urlparse(self.resource.url).hostname
-            # TODO: нужно сделать геттеры в ресурсе и кверях - очень неудобно это все расписывать
-            query_name = self.resource.query_name
-            date = datetime.now().date()
-            # TODO: формат вывода должен согласовываться с объектов лоадера - объект, который
-            path = f"{domain}/{query_name}/{date}_{query_name}.{storage.output_format}"
-        elif strg_cls == SqlRepo:
-            # TODO: для sql хранилищ нужно больше изучить сигнатуру функции to_sql - возможно там можно указать схему
-            # в которой нужно создать таблицу и тогда это нужно передавать в сторадж, а путь парсить как 'схема(кверя)/таблица(имя схемы)'
-            path = f"{self.resource.query_name}"
-
-        return path
-
-    def __init_subclass__(cls, crawl_type) -> None:
-        super().__init_subclass__(crawl_type)
+    @asynccontextmanager
+    async def watch_eor(self: Self) -> AsyncGenerator[None, None]:
+        try:
+            yield
+        except EndOfResource:
+            self.resource._next_url = True
 
 
-@define(slots=False)
-class ApiScraper(Scraper, crawl_type="api"):
-    client: AsyncClient = field(factory=AsyncClient)
+@dataclass
+class ApiScraper(Scraper, subcls_key="api"):
+    _: KW_ONLY
+    client_factory: Callable = field(default=ClientSession, kw_only=True)
+    batch_size: int = field(default=1)
+    use_proxy: bool = False
 
-    async def pending(self):
-        await self.trigger.pending()
+    def __post_init__(self, url: str):
+        self.resource = ApiResource(url)
 
-    @mark_as_trigger
-    async def scrape(self):
-        async with self.storage as repo:
-            async for url in self.resource.get_url_for():
-                print(url)
-                raw_data = await self.client.get(
-                    url, headers=self.extra_headers
-                )
-                # TODO: нужно сделать метод, позволяющий не делать цепочку вызовов к методу очистки данных
-                # и вообще переименовать его во что-то более/менее внятное
-                if raw_data.status_code == 200:
-                    content = raw_data.content
-                    tabular_data, sentinel = self.content_handler.handle(
-                        content
-                    )
-                    if sentinel:
-                        self.resource.sentinel = sentinel
-                        continue
-                    # TODO: здесь должна быть конкретная ошибка, по названию которой можно понять что произошло
-                    # Оставим либо так, либо организуем какой-то перехватчик на уровне цикла
-                    with suppress(ValueError):
-                        path = self.make_path(repo)
-                        await repo.write(tabular_data, path)
-                else:
-                    raise RuntimeError(
-                        f"Получен ненадлежащий код ответа {raw_data.status_code}. Проверьте дополнительные параметры запроса"
-                    )
+    async def process_requests(self: Self, urls: list[str]) -> list[bytes]:
+        session: type[ClientSession]
+        proxy = choice(proxy_list) if proxy_list and self.use_proxy else None  # nosec CWE-330
+        async with self.client_factory(
+            timeout=aiohttp.ClientTimeout(self.req_timeout),
+            headers=self.extra_headers,
+        ) as session:
+            tasks: list[Coroutine[Awaitable[Any], None, ClientResponse]] = [
+                session.get(url, proxy=proxy) for url in urls
+            ]  # type: ignore
+            responses: list[ClientResponse] = await gather(*tasks)
+        error_resp: ClientResponse
+        for error_resp in filterfalse(
+            lambda x: x.status in list(range(200, 299, 1)), responses
+        ):
+            msg: str = (await error_resp.content.read()).decode()
+            raise RuntimeError(error_resp.url, error_resp.headers, msg)
+        contents: list[bytes] = await gather(
+            *[resp.content.read() for resp in responses]
+        )
+        return contents
 
-    def __hash__(self) -> int:
-        return hash(id(self))
+    @as_trigger
+    async def scrape(self: Self) -> None:
+        async for urls in chunked(
+            self.resource.gen_url_for(), self.batch_size
+        ):
+            print(f"Process urls: {urls}")
+            context_anchor = self.resource.current_query
+            async with self.storage.start_stream(context_anchor) as repo:
+                async with self.watch_eor():
+                    content: list[bytes] = await self.process_requests(urls)
+                    await repo.put(content)
+                await sleep(self.req_delay)
