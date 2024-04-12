92898c2c3b9453a7bf5975ce5ee2a98adf2ba3ca
Ivan Danchuk <ivan.s.danchuk@gmail.com>
2024-04-12 12:29:58 +0300

    refactor(scheduling): added string parser
    The TimeCondition class now accepts strings like "12:00" in the activity
    start and end parameters by using the dateparser library.

--- src/yass/scheduling/timeinterval.py
+++ src/yass/scheduling/timeinterval.py
@@ -1,30 +1,52 @@
-from asyncio import sleep
+from asyncio import Condition, sleep
 from collections.abc import Iterable
-from dataclasses import InitVar, dataclass, field
+from dataclasses import KW_ONLY, InitVar, dataclass, field
 from datetime import datetime, time, timedelta
 from itertools import cycle
 from typing import Literal
 
-from .base import ActionCondition
+import dateparser
 
-__all__: list[str] = ["TimeCondition"]
+from yass.core import reg_type
+from yass.scheduling import ActionCondition
+
+__all__ = [
+    "AllowedWeekDays",
+    "AlwaysRun",
+    "DailyInterval",
+    "TimeCondition",
+    "WeekDaysType",
+    "WeekdayInterval",
+]
+
+
+class AlwaysRun(ActionCondition):
+    def __init__(self):
+        self.cond = Condition()
+
+    def is_able(self) -> bool:
+        return True
+
+    async def pending(self):
+        await self.cond.wait_for(lambda: self.is_able())
+        return self
 
 
 class DailyInterval:
     def __init__(
         self,
         day_interval: int,
-        start_time: time,
-        end_time: time,
-        launch: datetime | None = None,
+        start_time: str,
+        end_time: str,
+        launch: datetime | str | None = None,
     ):
         self._interval = day_interval
-        self.start = start_time
-        self.end = end_time
+        self.start: time = dateparser.parse(start_time).time()  # type:ignore
+        self.end: time = dateparser.parse(end_time).time()  # type: ignore
         self.launch: datetime = (
             datetime.combine(date=datetime.now().date(), time=self.start)
             if launch is None
-            else launch
+            else dateparser.parse(launch)  # type: ignore
         )
 
     def shift_launch(self, frequency: float):
@@ -43,7 +65,7 @@ class DailyInterval:
         return (
             datetime.now()
             >= self.launch
-            <= datetime.combine(date=self.launch.date(), time=self.end)
+            < datetime.combine(date=self.launch.date(), time=self.end)
         )
 
     def setted_period(self):
@@ -58,19 +80,19 @@ class WeekdayInterval:
     def __init__(
         self,
         weekday_interval: WeekDaysType,
-        start_time: time,
-        end_time: time,
-        launch: datetime | None = None,
+        start_time: str,
+        end_time: str,
+        launch: datetime | str | None = None,
     ):
         self._interval = weekday_interval
         self.weekday_it = cycle(set(sorted(weekday_interval)))
-        self.current_weekday = next(self.weekday_it)
-        self.start = start_time
-        self.end = end_time
+        self.current_weekday: AllowedWeekDays = next(self.weekday_it)
+        self.start: time = dateparser.parse(start_time).time()  # type: ignore
+        self.end: time = dateparser.parse(end_time).time()  # type: ignore
         self.launch: datetime = (
             datetime.combine(date=datetime.now().date(), time=self.start)
             if launch is None
-            else launch
+            else dateparser.parse(launch)  # type: ignore
         )
 
     def shift_launch(self, frequency):
@@ -88,29 +110,31 @@ class WeekdayInterval:
         return (
             datetime.now()
             >= self.launch
-            <= datetime.combine(date=self.launch.date(), time=self.end)
+            < datetime.combine(date=self.launch.date(), time=self.end)
         )
 
     def setted_period(self):
         return self._interval
 
 
-@dataclass(unsafe_hash=True)
+@reg_type("timer")
+@dataclass
 class TimeCondition(ActionCondition):
+    _: KW_ONLY
     period: InitVar[int | WeekDaysType] = field()
     # любая строка, которая может быть интепретирована как время. Датой считается текущая дата
-    start_time: InitVar[time] = field(default=time(0, 0, 1))
-    end_time: InitVar[time | None] = field(default=None)
+    start_time: InitVar[str] = field(default="0:01")
+    end_time: InitVar[str] = field(default="")
     frequency: float = field(default=0)
     launch_date: InitVar[datetime | None] = field(default=None)
     schedule_interval: DailyInterval | WeekdayInterval = field(init=False)
-    one_run: bool = field(default=False)
+    _one_run: bool = field(default=False, init=False)
 
     def __post_init__(self, period, start_time, end_time, launch_date):
-        if end_time is None:
+        if not end_time:
             hours: int = int(23 - (self.frequency // 1))
             minutes: int = int(59 - round((self.frequency % 1) * 60, 0))
-            end_time = time(hours, minutes)
+            end_time = time(hours, minutes).strftime("%H:%M:%S")
         if isinstance(period, int):
             self.schedule_interval = DailyInterval(
                 abs(period), start_time, end_time, launch_date
@@ -120,7 +144,7 @@ class TimeCondition(ActionCondition):
                 period, start_time, end_time, launch_date
             )
         if not self.frequency:
-            self.one_run = True
+            self._one_run = True
 
     def is_able(self):
         print("Start check condition")
@@ -134,7 +158,6 @@ class TimeCondition(ActionCondition):
             print(f"Condition was sleep above {delta} seconds")
             await sleep(delta.total_seconds())
         self.reset()
-        return self
 
     def get_delay(self) -> timedelta:
         current_datetime: datetime = datetime.now()
@@ -145,19 +168,16 @@ class TimeCondition(ActionCondition):
             return timedelta(seconds=0)
 
     def reset(self):
-        if self.schedule_interval.end <= datetime.now().time() or self.one_run:
+        self.schedule_interval.shift_launch(self.frequency)
+        # TODO: можно сократить проверку до вызова is_able и проверки на one_run - ниже условие по смыслу такое самое
+        if (
+            self.schedule_interval.end < self.schedule_interval.launch.time()
+            or self._one_run
+        ):
             self.schedule_interval.next_launch()
-        else:
-            self.schedule_interval.shift_launch(self.frequency)
 
-    @property
     def get_period(self):
         return self.schedule_interval.setted_period()
 
-    @property
     def get_next_run(self):
         return self.schedule_interval.launch
-
-
-if __name__ == "__main__":
-    ...
