fcf65b2cedd899aafddbaa4f2f0980737fbef98f
Ivan Danchuk <ivan.s.danchuk@gmail.com>
2024-04-12 14:32:53 +0300

    feat(resources): complete package refactoring
    The interfaces of all classes have been significantly expanded.
    The resource is now a global repository that defines the top-level
    parameters of calls to the data source and stores instances of requests
    to the resource.
    In addition to the request fields, the resource request class now also
    stores an activity trigger. The package introduces a new type of trigger
    - resource end trigger. It allows data collectors to know whether the
    data flow has ended during the next iteration of the data source.

--- src/yass/resources/api.py
+++ src/yass/resources/api.py
@@ -1,36 +1,75 @@
-from collections import defaultdict
+from __future__ import annotations
+
+from asyncio import Event, Lock
 from collections.abc import (
     AsyncGenerator,
     AsyncIterator,
-    Generator,
+    Callable,
     Iterable,
     Iterator,
+    MutableMapping,
+    MutableSequence,
 )
-from dataclasses import dataclass, field
-from functools import partial
-from itertools import zip_longest
-from typing import Any, NoReturn, Self, overload
+from itertools import count, zip_longest
+from typing import TYPE_CHECKING, Any, Literal, Self, cast, overload
 
 from aioitertools.itertools import product as async_product
+from rich.pretty import pprint as rpp
 
-from .base import BaseResource
+from yass.core import make_empty_instance, reg_type
+from yass.resources.base import (
+    ApiEORTrigger,
+    BaseResource,
+    BaseResourceRequest,
+)
+
+__all__ = [
+    "ApiRequest",
+    "ApiResource",
+    "BatchCounter",
+    "ContentLengthEORTrigger",
+    "MaxPageEORTrigger",
+    "StatusEORTrigger",
+]
+
+if TYPE_CHECKING:
+    from aiohttp import ClientResponse
+
+    from yass.contentio import IOContext
+    from yass.scheduling import ActionCondition, AlwaysRun
 
 
-@dataclass
-class ApiRequest:
-    part_name: str = field()
-    persist_fields: dict[str, str] = field(default_factory=dict)
-    mutable_fields: dict[str, list] = field(
-        default_factory=partial(defaultdict, list)
-    )
+@reg_type("api_req")
+class ApiRequest(BaseResourceRequest):
+    def __init__(
+        self,
+        name: str,
+        base_url: str,
+        persist_fields: MutableMapping[str, str],
+        mutable_fields: MutableMapping[str, MutableSequence],
+        io_context: IOContext,
+        collect_interval: ActionCondition = AlwaysRun(),
+        has_pages: bool = True,
+    ) -> None:
+        super().__init__(
+            name, base_url, io_context, collect_interval, has_pages
+        )
+        self.persist_fields: MutableMapping[str, str] = persist_fields
+        self.mutable_fields: MutableMapping[str, MutableSequence] = (
+            mutable_fields
+        )
+
+    def get_io_context(self) -> IOContext:
+        return self.io_context
+
+    def change_interval(self, interval: ActionCondition):
+        self.collect_interval: ActionCondition = interval
 
     @overload
-    def set_persist_field(self: Self, params: tuple[str, str]) -> None:
-        ...
+    def set_persist_field(self: Self, params: tuple[str, str]) -> None: ...
 
     @overload
-    def set_persist_field(self: Self, params: dict[str, str]) -> None:
-        ...
+    def set_persist_field(self: Self, params: dict[str, str]) -> None: ...
 
     def set_persist_field(self: Self, params: Iterable) -> None:
         if isinstance(params, (list, tuple)):
@@ -39,12 +78,10 @@ class ApiRequest:
             self.persist_fields.update(params)
 
     @overload
-    def set_mutable_field(self: Self, params: tuple[str, list]) -> None:
-        ...
+    def set_mutable_field(self: Self, params: tuple[str, list]) -> None: ...
 
     @overload
-    def set_mutable_field(self: Self, params: dict[str, list]) -> None:
-        ...
+    def set_mutable_field(self: Self, params: dict[str, list]) -> None: ...
 
     def set_mutable_field(self: Self, params: Iterable) -> None:
         if isinstance(params, (list, tuple)):
@@ -66,9 +103,9 @@ class ApiRequest:
 
     async def _build_mf_string(self: Self) -> AsyncGenerator[str, None]:
         if self.mutable_fields:
-            iterator: AsyncIterator[
-                tuple[tuple[str, Any | str], ...]
-            ] = self._build_mf_iterator()
+            iterator: AsyncIterator[tuple[tuple[str, Any | str], ...]] = (
+                self._build_mf_iterator()
+            )
             async for mutable_query in iterator:
                 mutable_part: str = "".join(
                     f"&{name}={value}" for name, value in mutable_query
@@ -93,53 +130,216 @@ class ApiRequest:
             full_string: str = persist_part + mutable_part
             yield full_string
 
-
-@dataclass
-class ApiResource(BaseResource, subcls_key="api"):
-    current_query: ApiRequest | None = field(default=None)
-    queries: dict[str, ApiRequest] = field(default_factory=dict)
-
-    @overload
-    def get_request(
-        self, *, name: str, autocreate: bool = False
-    ) -> ApiRequest | NoReturn:
-        ...
-
-    @overload
-    def get_request(self, *, name: str, autocreate: bool = True) -> ApiRequest:
-        ...
-
-    def get_request(self, *, name: str, autocreate: bool = True) -> ApiRequest:
-        try:
-            req: ApiRequest = self.queries[name]
-            return req
-        except KeyError as exc:
-            if autocreate:
-                req = ApiRequest(name)
-                self.queries[name] = req
-                return req
-            else:
-                raise KeyError from exc
-
-    async def gen_url_for(
-        self, request_name: str = ""
-    ) -> AsyncGenerator[str, None]:
-        if request_name:
-            queries: list[ApiRequest] = [
-                q for q in self.queries.values() if q.part_name == request_name
-            ]
-        else:
-            queries = list(self.queries.values())
-        while len(queries):
-            self.current_query = queries.pop()
-            print(f"Set query {self.current_query.part_name}")
-            async for query_part in self.current_query.build_full_query():
-                pager: Generator[int, Any, None] = self.page()
-                for page in pager:
-                    yield f"{self.url}{query_part}&page={page}"
-                    if self._next_url:
-                        self._next_url = False
+    async def gen_url(self) -> AsyncGenerator[str, Any]:
+        async for query_part in self.build_full_query():
+            base = f"{self.base_url}{query_part}"
+            if self.has_pages:
+                for page in count(1, 1):
+                    url = f"{base}&page={page}"
+                    sentinel: bool = yield url
+                    if sentinel:
                         break
+            else:
+                sentinel = yield base
+                if sentinel:
+                    break
+
+
+class MaxPageEORTrigger(ApiEORTrigger):
+    def __init__(
+        self,
+        *,
+        search_area: Literal["content", "headers"],
+        current_page_field: str,
+        max_page_field: str,
+    ):
+        self.fields: tuple[str, str] = (current_page_field, max_page_field)
+        self.search_area: str = search_area
+        self.content_handler: Callable = lambda x: x
+        self.search_type = search_area
+
+    def is_end_of_resource(self, response: ClientResponse | bytes) -> bool:
+        if self.search_area == "headers":
+            response = cast(ClientResponse, response)
+            return self._handle_headers(response)
+        else:
+            response = cast(bytes, response)
+            return self._handle_content(response)
+
+    def _handle_headers(self, response: ClientResponse) -> bool:
+        headers = response.headers
+        cur_page = cast(int, headers.get(self.fields[0]))
+        max_pages = cast(int, headers.get(self.fields[1]))
+        return int(cur_page) <= int(max_pages)
+
+    def _handle_content(self, response: bytes) -> bool:
+        content = self.content_handler(response)
+        rpp(
+            f"Обработано страниц {content[self.fields[0]]} из {content[self.fields[1]]}."
+        )
+        return int(content[self.fields[0]]) <= int(content[self.fields[1]])
+
+    def set_content_handler(self, func: Callable) -> None:
+        self.content_handler = func
+
+
+class StatusEORTrigger(ApiEORTrigger):
+    def __init__(self, status_code: int):
+        self.stop_status = status_code
+        self.search_type = "headers"
+
+    def is_end_of_resource(self, response: ClientResponse) -> bool:
+        status: int = response.status
+        return status == self.stop_status
+
+
+class ContentLengthEORTrigger(ApiEORTrigger):
+    def __init__(self, min_content_length: int):
+        self.stop_value = min_content_length
+        self.search_type = "headers"
+
+    def is_end_of_resource(self, response: ClientResponse) -> bool:
+        headers = response.headers
+        return int(headers.get("Content-Length")) <= self.stop_value  # type: ignore
+
+
+class BatchCounter:
+    def __init__(self, resource: ApiResource):
+        self.barrier: int = resource.max_batch
+        self._max_batch: int = resource.max_batch
+        self.active_tasks: int = 0
+        self.count_lock = Lock()
+        self.zero_control = Event()
+
+    @property
+    def min_batch(self):
+        return (
+            self._max_batch // self.active_tasks or 1,
+            self._max_batch % self.active_tasks or 1,
+        )
+
+    async def acquire_batch(self) -> int:
+        self.active_tasks += 1
+        rpp(
+            f"Количество активных задач на текущий момент {self.active_tasks}."
+        )
+        # async with self.count_lock:
+        async with self.count_lock:
+            rpp("Ожидаю освобождения блокировок.")
+            if self.barrier < min(self.min_batch):
+                await self.zero_control.wait()
+            rpp("Блокировки захвачены.")
+            acquire_size: int = max(self.barrier, *self.min_batch)
+            rpp(f"Захваченный лимит составляет {acquire_size}.")
+            self.barrier = self.barrier - acquire_size
+            rpp(f"Доступный лимит составляет {self.barrier}.")
+        self.zero_control.clear()
+        return acquire_size
+
+    def release_batch(self, current_size: int, recalc: bool = False):
+        self.barrier += current_size
+        rpp(
+            f"Высвободился лимит на {current_size} запросов. Доступный лимит составляет {self.barrier}."
+        )
+        if not recalc:
+            self.active_tasks -= 1
+
+    def recalc_limit(self, current_size: int) -> int:
+        if not self.count_lock.locked() and self.barrier >= 0:
+            new_batch: int = current_size + self.barrier
+            self.barrier = 0
+        else:
+            standart_batch: int = min(self.min_batch)
+            if (new_batch := current_size - standart_batch) > 0:
+                rpp(
+                    f"Пересчет размера батча. Текущий размер составляет {current_size}, избыток захваченных батчей составляет {standart_batch}."
+                )
+                self.release_batch(standart_batch, True)
+                self.zero_control.set()
+            else:
+                new_batch = current_size
+        return new_batch
+
+
+_EMPTY_EOR_TRIGGER: ApiEORTrigger = make_empty_instance(ApiEORTrigger)
+
+
+@reg_type("api")
+class ApiResource(BaseResource):
+    def __init__(
+        self,
+        url: str,
+        *,
+        extra_headers: dict = {},
+        eor_triggers: list[ApiEORTrigger] = [_EMPTY_EOR_TRIGGER],
+        max_batch: int = 1,
+        delay: int | float = 1,
+        request_timeout: int | float = 5,
+    ):
+        super().__init__(url, delay=delay, request_timeout=request_timeout)
+        self.max_batch: int = max_batch
+        self.batch = BatchCounter(self)
+        self.extra_headers: dict = extra_headers
+        self.eor_triggers: list[ApiEORTrigger] = eor_triggers
+
+    def configure(
+        self,
+        *,
+        extra_headers: dict | None = None,
+        max_batch: int | None = None,
+        delay: int | float | None = None,
+        eor_triggers: list[ApiEORTrigger] | None = None,
+    ) -> Self:
+        new_params = {
+            key: value
+            for key, value in locals().items()
+            if key != "self" and value is not None
+        }
+        self.batch.barrier = cast(int, max_batch)
+        default_params: dict[str, Any] = vars(self)
+        default_params.update(new_params)
+        for param, value in filter(
+            lambda x: hasattr(self, x[0]), default_params.items()
+        ):
+            setattr(self, param, value)
+        return self
+
+    def make_query(
+        self,
+        name: str,
+        persist_fields: MutableMapping[str, str],
+        mutable_fields: MutableMapping[str, MutableSequence],
+        io_context: IOContext,
+        collect_interval: ActionCondition = AlwaysRun(),
+        has_pages: bool = True,
+        replace: bool = False,
+    ) -> ApiRequest:
+        if name in self.queries and not replace:
+            msg = "Запрос к ресурсу с таким именем уже существует. Если требуется заменить запрос, установите 'replace=True'."
+            raise AttributeError(msg) from None
+        query = ApiRequest(
+            name,
+            self.url,
+            persist_fields,
+            mutable_fields,
+            io_context,
+            collect_interval,
+            has_pages,
+        )
+        self.queries[name] = query
+        return query
+
+    def get_query(self, name: str) -> ApiRequest:
+        return self.queries[name]  # type:ignore
+
+    def delete_query(self, name: str) -> None:
+        del self.queries[name]
+
+    def disable_query(self, name: str) -> None:
+        self.queries[name].enable = False
+
+    def enable_query(self, name: str) -> None:
+        self.queries[name].enable = True
 
 
 if __name__ == "__main__":
--- src/yass/resources/base.py
+++ src/yass/resources/base.py
@@ -1,24 +1,77 @@
+from __future__ import annotations
+
 from abc import abstractmethod
-from collections.abc import AsyncGenerator, Generator
-from contextlib import suppress
-from dataclasses import dataclass, field
-from typing import Any, Self
+from collections.abc import AsyncGenerator
+from typing import TYPE_CHECKING, Literal, Self, overload
 
-from ..core import YassCore
+from yass.core import YassCore
+
+if TYPE_CHECKING:
+    from aiohttp import ClientResponse
+
+    from yass.contentio import IOContext
+    from yass.scheduling import ActionCondition, AlwaysRun
+
+__all__ = ["ApiEORTrigger", "BaseResource", "BaseResourceRequest"]
 
 
-@dataclass
-class BaseResource(YassCore):
-    url: str
-    _next_url: bool = field(default=False)
-    max_pages: int = field(default=100, kw_only=True)
+class BaseResourceRequest(YassCore):
+    def __init__(
+        self,
+        name: str,
+        base_url: str,
+        io_context: IOContext,
+        collect_interval: ActionCondition = AlwaysRun(),
+        has_pages: bool = True,
+    ):
+        self.name: str = name
+        self.base_url: str = base_url
+        self.io_context = io_context
+        self.collect_interval = collect_interval
+        self.has_pages = has_pages
+        self.enable = True
 
     @abstractmethod
-    async def gen_url_for(
-        self: Self, query_name: str = ""
-    ) -> AsyncGenerator[str, None]:
-        ...
+    async def gen_url(self: Self) -> AsyncGenerator[str, None]: ...
 
-    def page(self) -> Generator[int, Any, None]:
-        with suppress(GeneratorExit):
-            yield from range(self.max_pages)
+    def get_io_context(self) -> IOContext:
+        return self.io_context
+
+
+class BaseResource(YassCore):
+    def __init__(
+        self,
+        url: str,
+        *,
+        delay: int | float = 1,
+        request_timeout: int | float = 5,
+    ):
+        self.url: str = url
+        self.delay: int | float = delay
+        self.request_timeout: int | float = request_timeout
+        self.queries: dict[str, BaseResourceRequest] = dict()
+
+    @abstractmethod
+    def configure(self) -> Self: ...
+    @abstractmethod
+    def make_query(self) -> BaseResourceRequest: ...
+    @abstractmethod
+    def get_query(self, name: str) -> BaseResourceRequest: ...
+    @abstractmethod
+    def delete_query(self, name: str) -> None: ...
+
+
+class ApiEORTrigger(YassCore):
+    def __init__(self):
+        self.search_type: Literal["content", "headers"]
+
+    @overload
+    @abstractmethod
+    def is_end_of_resource(self, response: ClientResponse) -> bool: ...
+
+    @overload
+    @abstractmethod
+    def is_end_of_resource(self, response: bytes) -> bool: ...
+
+    @abstractmethod
+    def is_end_of_resource(self, response: ClientResponse | bytes) -> bool: ...
